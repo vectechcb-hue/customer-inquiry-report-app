ee4011d40690f84f570071d23ec356fea9da2196
vectechcb-hue <vectech.cb@gmail.com>
2026-09-21 13:25:46 +0800

    Switch app to VECTECH customer inquiry v2 registration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const state={rows:[]};
 const GRAPH="https://graph.microsoft.com/v1.0";
-const CLIENT_ID="455752c7-007a-4bf2-b84a-249fd096126b";
+const CLIENT_ID="0a996912-7b43-4801-bcc8-ead9c3d03396";
 const TENANT_ID="12b9a8f7-aa9d-452b-b3f5-1369d0450558";
 const SCOPES=["User.Read","Mail.Read"];
 
